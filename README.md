b361d7f715adc909d816eb267fc898614ccc7966
Giovanni Pantuso <giopantuso@gmail.com>
2021-09-24 11:44:54 +0200

    Minor adjustements to README file.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# APPLIED OPERATIONS RESEARCH (AOR) 2020
+# APPLIED OPERATIONS RESEARCH (AOR) 2021
 This is the official Git repository of the course Applied Operations Research 2021 
 at the Department of Mathematical Sciences, University of Copenhagen.
 At this directory you will find tutorials, lectures and exercises.
@@ -7,4 +7,5 @@ Particularly, you will find the following
 - [A Python tutorial](python_material/python_tutorial.ipynb) and a number of [startup exercises](python_material/python_exercises.md). This tutorial, and the corresponding exercises, are particularly useful if you are not yet comfortable with the Python language.
 - [GAMS tutorials](gams_tutorials) which provide examples of usage of some of GAMS special features.
 - [Gurobi tutorials](gurobi_tutorials) which provide an introduction to the usage of Gurobi's Python interface.
+- [Class examples](class_examples) where you find the complete and commented version of the code illustrated during the lectures. 
 - [Solutions](solutions). In this folder you will find the code of the solutions of the exercises you will be given every week.  
---
+++ solutions/python_files/facility_sizing_version_2/fsp_mp.py
@@ -0,0 +1,55 @@
+from gurobipy import Model,GRB, quicksum
+from fsp_problem import FacilitySizingProblem
+
+class Master:
+
+    def __init__(self, fsp:FacilitySizingProblem):
+        self.fsp = fsp
+        self.m = Model()
+
+        # Creates the variables
+        self.x = self.m.addVars(fsp.n_facilities, vtype=GRB.CONTINUOUS, name="x")
+        self.phi = self.m.addVar(name="phi")
+
+        # Creates the objective
+        expr = self.phi + quicksum([self.fsp.fixed_costs[i] * self.x[i] for i in range(self.fsp.n_facilities)])
+        self.m.setObjective(expr, GRB.MINIMIZE)
+
+        # Creates the constraints
+        self.m.addConstrs(self.x[i] <= self.fsp.capacity[i] for i in range(fsp.n_facilities))
+
+    def solve(self):
+        self.m.optimize()
+
+    def get_solution(self):
+        '''
+        Returns two thing:
+        - the x solution (a list of values, one for each facility)
+        - the phi solution (a scalar).
+        Alternatively, one may create two methods, one that returns only x and one that returns only phi.
+        '''
+        return [self.x[i].x for i in range(self.fsp.n_facilities)], self.phi.x
+
+    def add_feasibility_cut(self,dualsCC:list,dualsDC:list):
+        '''
+        Adds a feasibility cut given the duals of the feasibility subproblem (extreme rays of polyhedral cone V)
+        '''
+        self.m.addConstr(quicksum([dualsCC[i] * self.x[i] for i in range(self.fsp.n_facilities)])
+                         <= - sum([dualsDC[j] * self.fsp.demands[j] for j in range(self.fsp.n_customers)]))
+        print("Added feasibility cut")
+
+    def add_optimality_cut(self,dualsCC:list,dualsDC:list):
+        '''
+        Adds an optimality cut given the duals of the optimality subproblem (extreme points of the dual of the optimality subproblem)
+        '''
+        self.m.addConstr(self.phi - quicksum([dualsCC[i] * self.x[i] for i in range(self.fsp.n_facilities)])
+                         >= sum([dualsDC[j] * self.fsp.demands[j] for j in range(self.fsp.n_customers)]))
+        print("Added optimality cut")
+
+    def print_solution(self):
+        for i in range(self.fsp.n_facilities):
+            print('%s %g' % (self.x[i].varName, self.x[i].x))
+        print('Obj: %g' % self.m.objVal)
+
+
+
